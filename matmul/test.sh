cef34ab8d0dfb5cbdc6f957a56fc6c5d19ecad7d
Ryedii <3024794255@qq.com>
2026-01-16 20:41:51 +0800

    feat: matmul

---
+++ matmul/test.sh
@@ -0,0 +1,7 @@
+workspace="/home/HwHiAiUser/wangty/lab5/matmul"
+
+mkdir AclNNInvocation/input
+mkdir AclNNInvocation/output
+# modify 'rm' to 'rm -f' in AclNNInvocation/run.sh
+
+bash AclNNInvocation/run.sh  # ex
